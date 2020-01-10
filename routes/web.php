c04ef95ecf9e300c279d5d0caa1bce5772614440
Neyelson Alves <neyelson@gmail.com>
2020-01-10 19:48:59 -0300

    Testando funcionalidades.

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
